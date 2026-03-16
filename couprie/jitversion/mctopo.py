d8389365aed3f9da6b9412ae2763a5e7d9d08835
koladik <koladik@gmail.com>
2026-03-16 21:29:23 +0300

    jitversion

---
+++ couprie/jitversion/mctopo.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numba import njit
+from .nbtopo import nbtopo
+
+@njit
+def pdestr4(image):
+    h, w = image.shape
+    destructible = np.zeros_like(image, dtype=np.uint8)
+
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            t4m, t4mm, t8p, t8pp = nbtopo(image, y, x)
+            if t4mm == 1 and t8p == 1:
+                destructible[y, x] = 1
+
+    return destructible
---
+++ couprie/jitversion/nbtopo.py
@@ -0,0 +1,46 @@
+import numpy as np
+from numba import njit
+from .topology import get_t4_zeros, get_t8_ones
+
+T4_ZEROS = get_t4_zeros().astype(np.uint8)
+T8_ONES = get_t8_ones().astype(np.uint8)
+
+@njit
+def nbtopo(image, y, x):
+    bitmask = get_bitmask_p(image, y, x)
+    t4mm = T4_ZEROS[bitmask]
+    t8p = T8_ONES[bitmask]
+
+    bitmask = get_bitmask_pp(image, y, x)
+    t4m = T4_ZEROS[bitmask]
+    t8pp = T8_ONES[bitmask]
+
+    return t4m, t4mm, t8p, t8pp
+
+@njit
+def get_bitmask_p(image, y, x):
+    c = image[y, x]
+    mask_p = 0
+    if image[y + 1, x    ] >= c: mask_p |= 1 << 0
+    if image[y + 1, x + 1] >= c: mask_p |= 1 << 1
+    if image[y    , x + 1] >= c: mask_p |= 1 << 2
+    if image[y - 1, x + 1] >= c: mask_p |= 1 << 3
+    if image[y - 1, x    ] >= c: mask_p |= 1 << 4
+    if image[y - 1, x - 1] >= c: mask_p |= 1 << 5
+    if image[y    , x - 1] >= c: mask_p |= 1 << 6
+    if image[y + 1, x - 1] >= c: mask_p |= 1 << 7
+    return mask_p
+
+@njit
+def get_bitmask_pp(image, y, x):
+    c = image[y, x]
+    mask_p = 0
+    if image[y + 1, x    ] > c: mask_p |= 1 << 0
+    if image[y + 1, x + 1] > c: mask_p |= 1 << 1
+    if image[y    , x + 1] > c: mask_p |= 1 << 2
+    if image[y - 1, x + 1] > c: mask_p |= 1 << 3
+    if image[y - 1, x    ] > c: mask_p |= 1 << 4
+    if image[y - 1, x - 1] > c: mask_p |= 1 << 5
+    if image[y    , x - 1] > c: mask_p |= 1 << 6
+    if image[y + 1, x - 1] > c: mask_p |= 1 << 7
+    return mask_p
