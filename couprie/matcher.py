321f66d2126abc6ef1ac5a3a6ad6089ddacb7ab4
koladik <koladik@gmail.com>
2026-03-16 21:28:16 +0300

    simmetric fix

--- couprie/matcher.py
+++ couprie/matcher.py
@@ -56,6 +56,85 @@ class Matcher:
         mask &= self.destroyable_view[(0, 0)] == DESTRUCTIBLE
         mask &= self.alpha_view[(1, 0)] < self.image_view[(0, 0)]
         self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
+        self.destroyable_view[(1, 0)][mask] = CRUCIAL_C
+
+        # (-1, 0)
+        mask = (
+                buffer[(-1, 0)] &
+                (buffer[(0, -1)] | buffer[(-1, -1)]) &
+                (buffer[(0, 1)] | buffer[(-1, 1)])
+
+        )
+        mask &= self.destroyable_view[(-1, 0)] == DESTRUCTIBLE
+        mask &= self.destroyable_view[(0, 0)] == DESTRUCTIBLE
+        mask &= self.alpha_view[(-1, 0)] < self.image_view[(0, 0)]
+        self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
+        self.destroyable_view[(-1, 0)][mask] = CRUCIAL_C
+
+        # (0, 1)
+        mask = (
+                buffer[(0, 1)] &
+                (buffer[(-1, 0)] | buffer[(-1, 1)]) &
+                (buffer[(1, 0)] | buffer[(1, 1)])
+        )
+        mask &= self.destroyable_view[(0, 1)] == DESTRUCTIBLE
+        mask &= self.destroyable_view[(0, 0)] == DESTRUCTIBLE
+        mask &= self.alpha_view[(0, 1)] < self.image_view[(0, 0)]
+        self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
+        self.destroyable_view[(0, 1)][mask] = CRUCIAL_C
+
+        # (0, -1)
+        mask = (
+                buffer[(0, -1)] &
+                (buffer[(-1, 0)] | buffer[(-1, -1)]) &
+                (buffer[(1, 0)] | buffer[(1, -1)])
+        )
+        mask &= self.destroyable_view[(0, -1)] == DESTRUCTIBLE
+        mask &= self.destroyable_view[(0, 0)] == DESTRUCTIBLE
+        mask &= self.alpha_view[(0, -1)] < self.image_view[(0, 0)]
+        self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
+        self.destroyable_view[(0, -1)][mask] = CRUCIAL_C
+
+    def match_c1(self):
+        """
+            [-1, 0, 1]
+            [-1, 1, 0]
+            [-1, 3, 3]
+        """
+        buffer = self.buffer
+        mask = buffer[(1, 1)] & (~buffer[(0, 1)] | ~buffer[(1, 1)])
+
+    def __init__(self, image, destroyable, alpha):
+        self.image = image
+        self.size = self.image.shape
+        self.bordered_size = (self.size[0] - 4, self.size[1] - 4)
+        self.image_view = self._build_views5x5(image)
+        self.destroyable = destroyable
+        self.destroyable_view = self._build_views5x5(self.destroyable)
+        self.alpha = alpha
+        self.alpha_view = self._build_views5x5(alpha)
+        self.buffer = self._create_buffer()
+
+
+    def match_c_asymmetric(self):
+        """
+            at least one of the numbered ones must be
+            [-1, 1, 1]
+            [-1, 2, 2]
+            [-1, 3, 3]
+            for forth directions
+        """
+        buffer = self.buffer
+
+        # (1, 0)
+        mask = (buffer[(1, 0)] &
+                (buffer[(0, 1)] | buffer[(1, 1)]) &
+                (buffer[(0, -1)] | buffer[(1, -1)])
+        )
+        mask &= self.destroyable_view[(1, 0)] == DESTRUCTIBLE
+        mask &= self.destroyable_view[(0, 0)] == DESTRUCTIBLE
+        mask &= self.alpha_view[(1, 0)] < self.image_view[(0, 0)]
+        self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
         #self.destroyable_view[(1, 0)][mask] = CRUCIAL_C
 
         # (-1, 0)
@@ -94,27 +173,6 @@ class Matcher:
         mask &= self.alpha_view[(0, -1)] < self.image_view[(0, 0)]
         self.destroyable_view[(0, 0)][mask] = CRUCIAL_C
         #self.destroyable_view[(0, -1)][mask] = CRUCIAL_C
-
-    def match_c1(self):
-        """
-            [-1, 0, 1]
-            [-1, 1, 0]
-            [-1, 3, 3]
-        """
-        buffer = self.buffer
-        mask = buffer[(1, 1)] & (~buffer[(0, 1)] | ~buffer[(1, 1)])
-
-    def __init__(self, image, destroyable, alpha):
-        self.image = image
-        self.size = self.image.shape
-        self.bordered_size = (self.size[0] - 4, self.size[1] - 4)
-        self.image_view = self._build_views5x5(image)
-        self.destroyable = destroyable
-        self.destroyable_view = self._build_views5x5(self.destroyable)
-        self.alpha = alpha
-        self.alpha_view = self._build_views5x5(alpha)
-        self.buffer = self._create_buffer()
-
 
     def _create_buffer(self):
         buffer = {}
