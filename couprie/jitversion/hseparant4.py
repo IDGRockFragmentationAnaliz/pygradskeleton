67681bf2a37f671ce1de29536712a3f3bfb8f7f1
koladik <koladik@gmail.com>
2026-04-08 14:38:35 +0300

    hseparant

--- couprie/jitversion/hseparant4.py
+++ couprie/jitversion/hseparant4.py
@@ -2,22 +2,29 @@ from numba import njit
 import numpy as np
 from .nbtopoh import nbtopoh
 from .nbtopo import nbtopo
+from .voisin import voisin
 
 @njit
 def separant4(image: np.ndarray, y, x):
     t4m, t4mm, t8p, t8pp = nbtopo(image, y, x)
     if t4mm >= 2:
-        return image[y, x]
+        return True
+    for k in range(8):
+        y2, x2 = voisin(y, x, k)
+        if image[y2, x2] < image[y, x]:
+            t4m, t4mm, t8p, t8pp = nbtopoh(image, y, x, image[y2, x2])
+            if t4mm >= 2:
+                return True
+    return False
 
-    return 0
 
 @njit
 def hseparant4(image: np.ndarray, y, x, h):
     if image[y, x] <= h:
-        return 0
+        return False
 
     t4m, t4mm, t8p, t8pp = nbtopo(image, y, x)
     if t4mm >= 2:
-        return 1
+        return True
 
-    return 0
+    return False
