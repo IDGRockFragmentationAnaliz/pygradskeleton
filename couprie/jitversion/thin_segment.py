50168499537d80005b617c6c95b6e32823350545
koladik <koladik@gmail.com>
2026-04-09 20:58:22 +0300

    thin segment fix

--- couprie/jitversion/thin_segment.py
+++ couprie/jitversion/thin_segment.py
@@ -1,6 +1,15 @@
 import numpy as np
 from numba import njit
-from .extensible4 import extensible4_all
+from .mctopo.hseparant4 import separant4
 
+@njit
 def thin_segment(image):
-    return extensible4_all(image)
+    height, width = image.shape
+    borders = np.zeros_like(image, dtype=np.uint8)
+
+    for y in range(1, height - 1):
+        for x in range(1, width - 1):
+            if separant4(image, y, x):
+                borders[y, x] = 255
+
+    return borders
