3e33207d02c060aa08e25abda001f548cf86b806
koladik <koladik@gmail.com>
2026-04-09 21:30:26 +0300

    delta4p created

---
+++ couprie/jitversion/mctopo/delta4p.py
@@ -0,0 +1,12 @@
+from numba import njit
+import numpy as np
+from .pconstr4 import pconstr4
+from .alpha8m import alpha8m
+
+def delta4p(image, y, x):
+    _saved = image[y, x]
+    while pconstr4(image, y, x):
+        image[y, x] = alpha8m(image, y, x)
+    res = image[y, x]
+    image[y, x] = _saved
+    return res
