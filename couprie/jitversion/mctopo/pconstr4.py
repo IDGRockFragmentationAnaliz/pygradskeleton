68494166eb8a0c0510bda3906d3e8151a5cb807b
koladik <koladik@gmail.com>
2026-04-09 21:13:49 +0300

    pconstr4 created

---
+++ couprie/jitversion/mctopo/pconstr4.py
@@ -0,0 +1,11 @@
+from numba import njit
+import numpy as np
+from .nbtopo import nbtopo
+
+def pconstr4(image, y, x):
+    t4m, t4mm, t8p, t8pp = nbtopo(image, y, x)
+
+    if t4m == 1 and t8pp == 1:
+        return 1
+    else:
+        return 0
