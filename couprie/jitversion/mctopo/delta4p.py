463cc7009d3463fc22ad26f6f68fb5b0d1399d11
koladik <koladik@gmail.com>
2026-04-21 18:27:10 +0300

    delta4p fix

--- couprie/jitversion/mctopo/delta4p.py
+++ couprie/jitversion/mctopo/delta4p.py
@@ -1,13 +1,13 @@
 from numba import njit
 import numpy as np
 from .pconstr4 import pconstr4
-from .alpha8m import alpha8m
+from .alpha8p import alpha8p
 
 @njit
 def delta4p(image, y, x):
     _saved = image[y, x]
-    while pconstr4(image, y, x):
-        image[y, x] = alpha8m(image, y, x)
+    while pconstr4(image, y, x) is True:
+        image[y, x] = alpha8p(image, y, x)
     res = image[y, x]
-    #image[y, x] = _saved
+    image[y, x] = _saved
     return res
