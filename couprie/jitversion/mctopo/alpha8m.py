c86eab3b6c1977189765d37f3b5cb12e463f2a54
koladik <koladik@gmail.com>
2026-04-21 18:26:56 +0300

    alpha 8m update

--- couprie/jitversion/mctopo/alpha8m.py
+++ couprie/jitversion/mctopo/alpha8m.py
@@ -1,47 +1,51 @@
-import numpy as np
 from numba import njit
-from .nbtopo import nbtopo
+import numpy as np
 
 
 @njit
 def alpha8m(image, y, x):
-    h, w = image.shape
     val = image[y, x]
-    alpha = -1
+    found = False
+    alpha = np.uint8(0)
 
-    v = image[y, x + 1] if x != w - 1 else val
-    if val > v > alpha:
+    v = image[y, x + 1]
+    if v < val:
         alpha = v
+        found = True
 
-    v = image[y - 1, x + 1] if (x != w - 1 and y != 0) else val
-    if val > v > alpha:
+    v = image[y - 1, x + 1]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y - 1, x] if y != 0 else val
-    if val > v > alpha:
+    v = image[y - 1, x]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y - 1, x - 1] if (y != 0 and x != 0) else val
-    if val > v > alpha:
+    v = image[y - 1, x - 1]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y, x - 1] if x != 0 else val
-    if val > v > alpha:
+    v = image[y, x - 1]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y + 1, x - 1] if (x != 0 and y != h - 1) else val
-    if val > v > alpha:
+    v = image[y + 1, x - 1]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y + 1, x] if y != h - 1 else val
-    if val > v > alpha:
+    v = image[y + 1, x]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    v = image[y + 1, x + 1] if (y != h - 1 and x != w - 1) else val
-    if val > v > alpha:
+    v = image[y + 1, x + 1]
+    if v < val and (not found or v > alpha):
         alpha = v
+        found = True
 
-    if alpha == -1:
-        return image[y, x]
-    else:
-        return np.uint8(alpha)
+    return alpha if found else val
