8078c6f3b0caf241b63d449b024ec37e1b81af80
koladik <koladik@gmail.com>
2026-04-21 18:31:42 +0300

    saddle4 init

---
+++ couprie/jitversion/mctopo/saddle4.py
@@ -0,0 +1,11 @@
+from numba import njit
+from .nbtopo import nbtopo
+
+
+def saddle4(image, y, x):
+    t4m, t4mm, t8p, t8pp = nbtopo(image, y, x)
+
+    if t8pp > 1 and t4mm > 1:
+        return True
+    else:
+        return False
