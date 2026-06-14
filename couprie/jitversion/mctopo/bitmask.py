40b308b757120da1778165750e5532cd80f0bb33
koladik <koladik@gmail.com>
2026-06-14 11:57:16 +0300

    flat bimask

--- couprie/jitversion/mctopo/bitmask.py
+++ couprie/jitversion/mctopo/bitmask.py
@@ -28,3 +28,17 @@ def bitmask_pp(image, y, x):
     if image[y    , x - 1] > c: mask_p |= 1 << 6
     if image[y + 1, x - 1] > c: mask_p |= 1 << 7
     return mask_p
+
+@njit(cache=True, inline="always")
+def bitmask_p_flat(image_flat, idx, w):
+    c = image_flat[idx]
+    mask_p = np.uint8(0)
+    if image_flat[idx + w] >= c: mask_p |= np.uint8(1 << 0)
+    if image_flat[idx + w + 1] >= c: mask_p |= np.uint8(1 << 1)
+    if image_flat[idx + 1] >= c: mask_p |= np.uint8(1 << 2)
+    if image_flat[idx - w + 1] >= c: mask_p |= np.uint8(1 << 3)
+    if image_flat[idx - w] >= c: mask_p |= np.uint8(1 << 4)
+    if image_flat[idx - w - 1] >= c: mask_p |= np.uint8(1 << 5)
+    if image_flat[idx - 1] >= c: mask_p |= np.uint8(1 << 6)
+    if image_flat[idx + w - 1] >= c: mask_p |= np.uint8(1 << 7)
+    return mask_p
