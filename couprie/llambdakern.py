7f98e0e0a55c4396c88f0102cb1c73215937d925
koladik <koladik@gmail.com>
2026-06-27 04:22:41 +0300

    sparce lambdakern update

--- couprie/llambdakern.py
+++ couprie/llambdakern.py
@@ -14,17 +14,14 @@ def llambdakern(image, lam, copy = True, progress=False):
     seen_rect = np.ones(image.shape, np.uint8)
     set_edge_zeros(seen_rect)
     seen = seen_rect.ravel()
-
-    # for y in range(1, height - 1):
-    #     for x in range(1, width - 1):
-    #         seen[y * width + x] = 1
+    sparce = np.flatnonzero(seen)
     head = (height - 2) * (width - 2)
 
     pbar = _make_progress(head, progress)
     first_iteration = True
 
     while head > 0:
-        head = _llambdakern_loop(image, seen, head, lam)
+        head = _llambdakern_loop(image, seen, sparce, head, lam)
         if pbar is not None:
             pbar.n = head
             if first_iteration:
@@ -34,26 +31,30 @@ def llambdakern(image, lam, copy = True, progress=False):
     return image
 
 @njit(cache=True)
-def _llambdakern_loop(image, seen, head, lam):
+def _llambdakern_loop(image, seen, sparce, head, lam):
     height, width = image.shape
-    for y in range(1, height - 1):
-        for x in range(1, width - 1):
-            p = y * width + x
 
-            if seen[p] == 1:
-                seen[p] = 0
-                head -= 1
 
-                if abaisse4(image, y, x, lam):
-                    for k in range(8):
-                        qy, qx = voisin(y, x, k)
-                        q = qy * width + qx
+    for i in range(sparce.size):
+        p = sparce[i]
 
-                        if qy <= 0 or qy >= height - 1 or qx <= 0 or qx >= width - 1:
-                            seen[q] = 0
-                        elif seen[q] == 0:
-                            seen[q] = 1
-                            head += 1
+        y = p // width
+        x = p % width
+
+        if seen[p] == 1:
+            seen[p] = 0
+            head -= 1
+
+            if abaisse4(image, y, x, lam):
+                for k in range(8):
+                    qy, qx = voisin(y, x, k)
+                    q = qy * width + qx
+
+                    if qy <= 0 or qy >= height - 1 or qx <= 0 or qx >= width - 1:
+                        seen[q] = 0
+                    elif seen[q] == 0:
+                        seen[q] = 1
+                        head += 1
 
     return head
 
